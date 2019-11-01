851c7681f1fcbb02125efe32b344663c7ddec553
gera09 <bp-sva@yandex.ru>
2019-11-01 15:32:03 +0300

    таблицы делаю

--- Project_weather_rp5/01_schema.sql
+++ Project_weather_rp5/01_schema.sql
@@ -15,8 +15,6 @@ DROP TABLE IF EXISTS report_27;
 CREATE TABLE report_27 (
 	id SERIAL PRIMARY KEY,
 	
-	
-	
 	-- trading_date DATE COMMENT 'Дата, пример в отчете (2019-10-13)', -- STR_TO_DATE() --https://incode.pro/mysql/rabota-s-datami-v-mysql.html ======================
 	-- trading_hour TINYINT COMMENT 'Торговый час (00 или 23), пример в отчете (01-02)',	-- объединить с датой в datetime -- 1 =====================================
 	trading_date DATETIME null, -- STR_TO_DATE('2019-10-13 00', '%d.%m.%Y %H:%i:%s'), -- 2019-10-13 - дата, 00 - час          -- 1
@@ -70,7 +68,7 @@ CREATE TABLE report_28 (
 	trading_date DATETIME null, -- STR_TO_DATE('2019-10-13 00', '%d.%m.%Y %H:%i:%s') -- 2019-10-13 - дата, 00 - час          -- 1
 	-- select STR_TO_DATE('2019-10-13 00', '%Y-%m-%d %H:%i:%s')
 	
-	gtp VARCHAR(8) COMMENT 'GVIE0001',
+	gtpp VARCHAR(8) COMMENT 'PVIE0001',
 	
 	-- stair FLOAT COMMENT 'Заявка субъекта - Ступень', -- пустая колонка												 -- 2
 	-- p_stair FLOAT COMMENT 'Заявка субъекта - Цена руб/(МВт*час)', -- пустая колонка								 	 -- 3
@@ -114,79 +112,40 @@ CREATE TABLE report_28 (
 	
 	-- индексы
 	index(trading_date),
-	index(gtp),
+	index(gtpp),
     
     -- прописать ключи
-    -- FOREIGN KEY (to_user_id) REFERENCES users(id)
        FOREIGN KEY (trading_date) REFERENCES report_27(trading_date), -- ON DELETE RESTRICT ON UPDATE RESTRICT
-       FOREIGN KEY (gtp) REFERENCES report_27(gtp)
+       FOREIGN KEY (gtpp) REFERENCES report_27(gtp)
 );
 
 
 DROP TABLE IF EXISTS br;
 CREATE TABLE br (
 
-
-/*========================================================== stop here!!! */
-/* table's name:
- * --br
- * forecast_rp5
- * fact_rp5
- * insol
- * fin_rez
- * ku
- * peni_mounth
- * peni_kontrag
- * */
-
-
-
 	id SERIAL PRIMARY KEY,
-	-- trading_date DATE COMMENT 'Дата, пример в отчете (2019-10-13)', -- STR_TO_DATE() --https://incode.pro/mysql/rabota-s-datami-v-mysql.html
-	-- trading_hour TINYINT COMMENT 'Торговый час (00 или 23), пример в отчете (01-02)',	-- объединить с датой в datetime 
-	trading_date DATETIME null, -- STR_TO_DATE('2019-10-13 00', '%d.%m.%Y %H:%i:%s') -- 2019-10-13 - дата, 00 - час          -- 1
-	-- select STR_TO_DATE('2019-10-13 00', '%Y-%m-%d %H:%i:%s')
+	trading_date DATETIME null, -- STR_TO_DATE('2019-10-13 00', '%d.%m.%Y %H:%i:%s') -- 2019-10-13 - дата, 00 - час          
 	
 	gtp VARCHAR(8) COMMENT 'GVIE0001',
-	
-	-- stair FLOAT COMMENT 'Заявка субъекта - Ступень', -- пустая колонка												 -- 2
-	-- p_stair FLOAT COMMENT 'Заявка субъекта - Цена руб/(МВт*час)', -- пустая колонка								 	 -- 3
-	-- v_bid_so FLOAT UNSIGNED COMMENT 'Заявка субъекта - Заявленный объем в АТС МВт*час',								 -- 4
-	-- v_bid_so_korr FLOAT UNSIGNED COMMENT 'Заявка субъекта - Скорректированный АТС объем заявки МВт*час',				 -- 5
-	-- v_losses_load_prog FLOAT COMMENT 'Объем прогнозных нагрузочных потерь в сетях, включенных в данную ГТП, МВт*час'  -- 6
-	
-	v_SO_zayav FLOAT UNSIGNED COMMENT 'Заявленный объем в СО',															 -- 7
-	-- v_rd -- у нас нет РД -- пустая колонка																			 -- 8
-	
-	trade_graph FLOAT UNSIGNED COMMENT 'Объем Торгового графика МВт*час',												 -- 9
-	p_unreg FLOAT UNSIGNED COMMENT 'Средневзвешенная цена на продажу (по принятым поузловым объемам) руб/(МВт*час)', 	 -- 10
-	
-	-- Полный отнесённый на ГТПП объём генерации блок-станций МВт*час -- нулевая колонка							   	 -- 11	
-
-	tr_graph_gp FLOAT UNSIGNED COMMENT 'Объем ТГ за вычетом блок-станций, с учётом отнесения объёмов розничных договоров на ГП (ТГ*)МВт*час',	-- 12
-	
-	-- РСВ
-	v_buy_rsv FLOAT UNSIGNED COMMENT 'Покупка в РСВ - Объем покупки МВт*час',											-- 13
-	-- В т.ч. потери, отнесенные к объему покупки -- нулевая колонка  													-- 14
-	p_buy_rsv FLOAT UNSIGNED COMMENT 'Покупка в РСВ - Цена руб/(МВт*час)',												-- 15
-	
-	-- v_sell_rsv FLOAT UNSIGNED COMMENT 'Продажа в обеспечение РД - Объем покупки МВт*час',							-- 16
-	-- p_sell_rsv FLOAT UNSIGNED COMMENT 'Продажа в обеспечение РД - Цена руб/(МВт*час)',								-- 17
-	
-	-- СДД
-	v_sell_sdd FLOAT UNSIGNED COMMENT 'Продажа в обеспечение СДД (СДЭМ) - Объем покупки МВт*час',						-- 18
-	p_sell_sdd FLOAT UNSIGNED COMMENT 'Продажа в обеспечение СДД (СДЭМ) - Цена руб/(МВт*час)',							-- 19
-	-- v_buy_sdd FLOAT UNSIGNED COMMENT 'Покупка в обеспечение СДД (СДЭМ) - Объем продажи МВт*час',						-- 20
-	-- p_buy_sdd FLOAT UNSIGNED COMMENT 'Покупка в обеспечение СДД (СДЭМ) - Цена руб/(МВт*час)',						-- 21
-	
-	-- Продажа перевышения генерации блок-станций над потреблением - Объем покупки МВт*час	-- пустая колонка			-- 22
-	-- Продажа перевышения генерации блок-станций над потреблением - Цена руб/(МВт*час)		-- пустая колонка			-- 23
-	-- Объем нагрузочных потерь, отнесенных на данную ГТП, МВт*час 							-- пустая колонка			-- 24
-	-- Объем нагрузочных потерь в ЭР участника, определенных по результатам конкурентного отбора МВт*час -- пустая кол  -- 25
-	-- Стоимость нагрузочных потерь, учтенных в равновесных ценах, руб						-- пустая колонка			-- 26
-	-- korr_stair UNSIGNED COMMENT 'Скорректированная заявка субъекта - Ступень'										-- 27
-    -- korr_p UNSIGNED COMMENT 'Скорректированная заявка субъекта - Количество МВт*час' 								-- 28
-    -- korr_v FLOAT UNSIGNED COMMENT 'Скорректированная заявка субъекта - Цена руб/(МВт*час)'							-- 29
+		
+	tg FLOAT UNSIGNED COMMENT 'торговый график',															 
+	PminPDG FLOAT UNSIGNED,																		 
+	PmaxPDG  FLOAT UNSIGNED,	
+	PVsvgo FLOAT UNSIGNED,	
+	PminVsvgo FLOAT UNSIGNED,	
+	PmaxVsvgo FLOAT UNSIGNED,	
+	PminBR FLOAT UNSIGNED,	
+	PmaxBR FLOAT UNSIGNED,	
+	IBR FLOAT UNSIGNED,	
+	CbUP FLOAT UNSIGNED,	
+	CbDown FLOAT UNSIGNED,	
+	CRSV FLOAT UNSIGNED,	
+	TotalBR FLOAT UNSIGNED,	
+	EVR FLOAT UNSIGNED,	
+	OCPU FLOAT UNSIGNED,	
+	OCPS FLOAT UNSIGNED,	
+	Pmin FLOAT UNSIGNED,	
+	Pmax FLOAT UNSIGNED,	
 	
 	
 	-- индексы
@@ -200,7 +159,117 @@ CREATE TABLE br (
 );
 
 
+DROP TABLE IF EXISTS fact_rp5;
+CREATE TABLE fact_rp5 (
 
+	id SERIAL PRIMARY KEY,
+	trading_date DATETIME null, -- STR_TO_DATE('2019-10-13 00', '%d.%m.%Y %H:%i:%s') -- 2019-10-13 - дата, 00 - час          
+	gtpp VARCHAR(8) COMMENT 'PVIE0001',
+		
+	local_time DATETIME null COMMENT 'местн. время',-- STR_TO_DATE('2019г. 1 ноября 16:00', '%d.%m.%Y %H:%i:%s') -часть после заяпятой  не адаптирована														 
+	temp FLOAT UNSIGNED,																		 
+	humid  FLOAT unsigned  COMMENT 'влажность',	
+	w_dir FLOAT unsigned COMMENT 'направление ветра',	
+	w_speed FLOAT unsigned COMMENT 'скорость ветра',
+	w_gusts FLOAT unsigned COMMENT 'порывы ветра',
+	ww VARCHAR(1000)  COMMENT 'явления погоды',
+	www VARCHAR(1000) COMMENT 'оперативные явления погоды',
+	сloudy FLOAT unsigned COMMENT 'общая облачность', 				-- привести от текста к цифрам
+	vv FLOAT unsigned COMMENT 'горизонтальная дальность видимости',	-- привести от текста к цифрам
+	td FLOAT unsigned COMMENT 'точка росы',	
+	
+	
+	
+	-- индексы
+	index(trading_date),
+	index(gtpp),
+	index(local_time),
+    
+    -- прописать ключи
+    -- FOREIGN KEY (to_user_id) REFERENCES users(id)
+       FOREIGN KEY (trading_date) REFERENCES report_27(trading_date),
+       FOREIGN KEY (gtpp) REFERENCES report_27(gtp) 
+);
+
+
+DROP TABLE IF EXISTS forecast_rp5;
+CREATE TABLE forecast_rp5 (
+
+	id SERIAL PRIMARY KEY,
+	trading_date DATETIME null, -- STR_TO_DATE('2019-10-13 00', '%d.%m.%Y %H:%i:%s') -- 2019-10-13 - дата, 00 - час          
+	gtpp VARCHAR(8) COMMENT 'PVIE0001',
+		
+	local_time DATETIME null COMMENT 'местн. время',-- STR_TO_DATE('2019г. 1 ноября 16:00', '%d.%m.%Y %H:%i:%s') -часть после заяпятой  не адаптирована														 
+	cloudy_v FLOAT unsigned  COMMENT 'облака вертикального развития',
+	cloudy_l FLOAT unsigned  COMMENT 'облака нижнего яруса',
+	cloudy_m FLOAT unsigned  COMMENT 'облака среднего яруса',
+	cloudy_h FLOAT unsigned  COMMENT 'облака верхнего яруса',
+	hrecip VARCHAR(1000)  COMMENT 'осадки описание (снег/дождь)',
+	hrecip_level FLOAT unsigned  COMMENT 'осадки количество мм',
+	ww VARCHAR(1000)  COMMENT 'явления погоды', -- туман, дымка
+	vis FLOAT unsigned  COMMENT 'горизонтальная видимость', -- крайние часы интерполировать для плавного перехода? (1 - 2,75 - 4,5)
+	temp FLOAT UNSIGNED,
+	w_speed FLOAT unsigned COMMENT 'скорость ветра',
+	w_gusts FLOAT unsigned COMMENT 'порывы ветра',
+	w_dir VARCHAR(50) COMMENT 'направление ветра',
+	humid  FLOAT unsigned  COMMENT 'влажность',
+	
+	-- индексы
+	index(trading_date),
+	index(gtpp),
+	index(local_time),
+    
+    -- прописать ключи
+    FOREIGN KEY (trading_date) REFERENCES report_27(trading_date),
+    FOREIGN KEY (gtpp) REFERENCES report_27(gtp) 
+);
+
+DROP TABLE IF EXISTS insol;
+CREATE TABLE insol (
+
+	id SERIAL PRIMARY KEY,
+	trading_date DATETIME null, -- STR_TO_DATE('2019-10-13 00', '%d.%m.%Y %H:%i:%s') -- 2019-10-13 - дата, 00 - час          
+	gtpp VARCHAR(8) COMMENT 'PVIE0001',
+	
+	/*========================================================== stop here!!! */
+/* table's name:
+ * insol
+ * links_meteo_lok
+ * links_obj_insol
+ * ku
+ * 
+ * fin_rez
+ * peni_mounth
+ * peni_kontrag
+ * */
+	id_param FLOAT unsigned  COMMENT 'ID параметра на сайте Grafanya',
+	name VARCHAR(50)  COMMENT 'наименование значения',
+	value DOUBLE COMMENT 'само значение',
+	
+	
+	cloudy_v FLOAT unsigned  COMMENT 'облака вертикального развития',
+	cloudy_l FLOAT unsigned  COMMENT 'облака нижнего яруса',
+	cloudy_m FLOAT unsigned  COMMENT 'облака среднего яруса',
+	cloudy_h FLOAT unsigned  COMMENT 'облака верхнего яруса',
+	hrecip VARCHAR(1000)  COMMENT 'осадки описание (снег/дождь)',
+	hrecip_level FLOAT unsigned  COMMENT 'осадки количество мм',
+	ww VARCHAR(1000)  COMMENT 'явления погоды', -- туман, дымка
+	vis FLOAT unsigned  COMMENT 'горизонтальная видимость', -- крайние часы интерполировать для плавного перехода? (1 - 2,75 - 4,5)
+	temp FLOAT UNSIGNED,
+	w_speed FLOAT unsigned COMMENT 'скорость ветра',
+	w_gusts FLOAT unsigned COMMENT 'порывы ветра',
+	w_dir VARCHAR(50) COMMENT 'направление ветра',
+	humid  FLOAT unsigned  COMMENT 'влажность',
+	
+	-- индексы
+	index(trading_date),
+	index(gtpp),
+	index(local_time),
+    
+    -- прописать ключи
+    FOREIGN KEY (trading_date) REFERENCES report_27(trading_date),
+    FOREIGN KEY (gtpp) REFERENCES report_27(gtp) 
+);
 
 -- ALTER TABLE sql_project.report_27 ADD CONSTRAINT report_27_fk FOREIGN KEY (trading_date) REFERENCES sql_project.report_28(trading_date) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
