88c4109639741fc35bb7a195880932d4e9864ddd
gera09 <bp-sva@yandex.ru>
2019-11-04 23:29:51 +0300

    см

--- dz06/procedures.sql
+++ dz06/procedures.sql
@@ -29,3 +29,87 @@ show procedure status like 'my_version';
 select * from mysql.`user` limit 10;
 
 show create procedure example.my_version;
+
+
+
+-- с модификатором IN @y НЕ будет изменен!!!
+DROP procedure IF EXISTS example.set_x;
+DELIMITER //
+create procedure example.set_x(IN value INT) -- IN - будет по умолчанию, если не указывать
+begin
+	set @x = value;
+	set value = value-1000;
+end //
+DELIMITER ;
+
+call example.set_x(12345);
+select @x;
+
+set @y = 10000;
+call example.set_x(@y);
+select @x, @y;
+
+
+
+-- с модификатором OUT @y БУДЕТ изменен!!!
+DROP procedure IF EXISTS example.set_x;
+DELIMITER //
+create procedure example.set_x(OUT value INT) -- OUT
+begin
+	set @x = value;
+	set value = 1000; -- если оставить по-старому, то будет то будет @y = NULL, потому что не INOUT
+end //
+DELIMITER ;
+
+-- call example.set_x(12345); -- такой вызов на OUT не пройдет!
+
+set @y = 10000;
+call example.set_x(@y);
+select @x, @y;
+
+
+
+-- с модификатором INOUT @y все удет работать!!!
+DROP procedure IF EXISTS example.set_x;
+DELIMITER //
+create procedure example.set_x(INOUT value INT) -- INOUT
+begin
+	set @x = value;
+	set value = value- 1000; -- работает!
+end //
+DELIMITER ;
+
+-- call example.set_x(12345); -- такой вызов на OUT не пройдет!
+
+set @y = 10000;
+call example.set_x(@y);
+select @x, @y;
+
+
+
+
+-- DECLARE
+DROP procedure IF EXISTS example.DECLARE_var;
+DELIMITER //
+create procedure example.DECLARE_var()
+begin
+	DECLARE var tinytext default 'внешняя переменная'; -- эта переменая будет доступна и во внутреннем блоке (видно будет, если закомментить строку 'внутренняя переменная')
+	begin		
+		DECLARE var tinytext default 'внутренняя переменная';
+		select var;
+	end;
+	select var;
+end//
+DELIMITER ;
+
+call example.DECLARE_var();
+
+
+
+
+
+
+
+
+
+
